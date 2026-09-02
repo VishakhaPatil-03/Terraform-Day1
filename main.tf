d2619247e0398d97309a8e863820a9ffacb33c33
VishakhaPatil <vishakhapatil1441@gmail.com>
2026-09-02 16:35:12 +0530

    sg chnage done

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 resource "aws_instance" "day1" {
-    name = "day1"
     ami = variable.aws_ami
     instance_type = variable.instance_type
     user_data = file("/home/ubuntu/Terraform-Day1/user.sh")
@@ -18,27 +17,27 @@ resource "aws_security_group" "sg" {
     description = "Security group for day1 instance"
    
    ingress {
-      form_port = 22
+      from_port = 22
       to_port = 22
       protocol = "tcp"
       cidr_blocks = ["0.0.0.0/0"]
    }
    ingress {
-    form_port = 80
+    from_port = 80
     to_port = 80
     protocol = "http"
     cidr_blocks = ["0.0.0.0/0"]
    }
 
    ingress {
-    form_port = 443
+    from_port = 443
     to_port = 443
     protocol = "https"
     cidr_blocks = ["0.0.0.0/0"]
    }
 
    egress {
-    form_port = 0
+    from_port = 0
     to_port = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
