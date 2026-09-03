3f990385c0161e778b86b74112864075c90162d0
VishakhaPatil <vishakhapatil1441@gmail.com>
2026-09-03 17:07:16 +0530

    done

--- Day2/main.tf
+++ Day2/main.tf
@@ -63,12 +63,12 @@ tags = {
     }
 }
 
-resource "aws_subnet_route_table_association" "public_subnet_assoc" {
+resource "aws_route_table_association" "public_subnet_assoc" {
     subnet_id = aws_subnet.public_subnet.id
     route_table_id = aws_route_table.public_rt.id
 }
 
-resource "aws_subnet_route_table_association" "private_subnet_assoc" {
+resource "aws_route_table_association" "private_subnet_assoc" {
     subnet_id = aws_subnet.private_subnet.id
     route_table_id = aws_route_table.public_rt.id
 }
