a008a60258d4058c7b519c2172027eb46fba6776
VishakhaPatil <vishakhapatil1441@gmail.com>
2026-09-05 16:39:09 +0530

    chnage done

--- Day4/module/ec2/main.tf
+++ Day4/module/ec2/main.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "public_instance" {
     ami = var.ami
     instance_type = var.instance_type
     key_name = var.key_name
-    vpc_security_group_ids = [var.sg_id]
+    vpc_security_group_ids = [var.public_subnet_id]
     user_data = file("/home/ubuntu/Terraform-Day1/Day4/module/ec2/user_data.sh")
     subnet_id = var.public_subnet_id
     associate_public_ip_address =  true
@@ -15,7 +15,7 @@ resource "aws_instance" "private_instance" {
     ami = var.ami
     instance_type = var.instance_type
     key_name = var.key_name
-    vpc_security_group_ids = [var.sg_id]
+    vpc_security_group_ids = [var.private_subnet_id]
     user_data = file("/home/ubuntu/Terraform-Day1/Day4/module/ec2/user_data.sh")
     subnet_id = var.private_subnet_id
     
