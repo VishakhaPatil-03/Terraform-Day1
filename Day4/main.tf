5b7d508918aac43434d2d6155b50cd75b2066eec
VishakhaPatil <vishakhapatil1441@gmail.com>
2026-09-05 16:25:45 +0530

    done

--- Day4/main.tf
+++ Day4/main.tf
@@ -12,7 +12,7 @@ module "vpc" {
 
 module "ec2"{
     source = "./module/ec2"
-    ami = "ami-0532913178263be1"
+    ami = "ami-0532913178263be11"
     instance_type = "t3.micro"
     key_name = "kapishh"
     sg_id = module.vpc.vpc_id
