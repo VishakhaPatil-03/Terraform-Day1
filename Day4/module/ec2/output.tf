2f84dcd089714f83718dc591b2c8dcc1e01b1fd7
VishakhaPatil <vishakhapatil1441@gmail.com>
2026-09-05 16:18:04 +0530

    done

--- Day4/module/ec2/output.tf
+++ Day4/module/ec2/output.tf
@@ -1,12 +1,13 @@
-output "public_ip"{
-    value = aws_instance.public_instance.public_ip.id
 
+
+output "public_ip" {
+  value = aws_instance.public_instance.public_ip
 }
 
-output "private_ip"{
-    value = aws_instance.private_instance.private_ip.id
+output "private_ip" {
+  value = aws_instance.private_instance.private_ip
 }
 
-output "public_ip_DNS"{
-    value = aws_instance.public_instance.public_dns.id
+output "public_ip_DNS" {
+  value = aws_instance.public_instance.public_dns
 }
